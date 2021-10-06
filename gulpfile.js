4719e75ec653ee28985440bb45fcaafbe837d4f9
Olalekan Olonilua <oloniluaolalekan@gmail.com>
2021-10-06 16:35:33 +0100

    Gulp Part 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,32 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    del = require('del'),
+    browserSync = require('browser-sync'),
+    imagemin = require('gulp-imagemin'),
+    //more
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
 
-    gulp.task('sass', function () {
+gulp.task('sass', function () {
         return gulp.src('./css/*.scss')
           .pipe(sass().on('error', sass.logError))
           .pipe(gulp.dest('./css'));
       });
+
       /*
 
       gulp.task('sass:watch', function () {
         gulp.watch('./css/*.scss', ['sass']);
       });  
       */
-      gulp.task('sass:watch', function(){
+   gulp.task('sass:watch', function(){
     gulp.watch('./css/*.scss', gulp.series('sass'));
-});
+      });
 
       gulp.task('browser-sync', function () {
         var files = [
@@ -34,9 +44,55 @@ var gulp = require('gulp'),
         });
      
      });
+
+// Clean
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+gulp.task('copyfonts', function () {
+    return gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+        .pipe(gulp.dest('./dist/fonts'));
+});
+
+// Images
+gulp.task('imagemin', function() {
+  return gulp.src('img/*.{png,jpg,gif}')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function () {
+    return gulp.src('./*.html')
+        .pipe(flatmap(function (stream, file) {
+            return stream
+                .pipe(usemin({
+                    css: [rev()],
+                    html: [function () {
+                        return htmlmin({
+                            collapseWhitespace: true
+                        })
+                    }],
+                    js: [uglify(), rev()],
+                    inlinejs: [uglify()],
+                    inlinecss: [cleanCss(), 'concat']
+                }))
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+/*gulp.task('build',['clean'], function() {
+    gulp.start('copyfonts','imagemin');
+});*/
       
       // Default task
      
      gulp.task('default', gulp.parallel('sass:watch', 'browser-sync', function() {
     gulp.series('sass:watch')();
 }));
+gulp.task('build', gulp.series(
+    'clean',
+    'copyfonts',
+    'imagemin',
+    'usemin'
+));
